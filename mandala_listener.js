75e8b4c552e9cf75a9044ecbb9bd3e92f1365c7a
OfferZen-Maker <make@offerzen.com>
2018-07-31 16:37:35 +0200

    WIP

--- mandala_listener.js
+++ mandala_listener.js
@@ -2,24 +2,77 @@ const awsIot = require('aws-iot-device-sdk');
 const moment = require('moment'); // for DateTime formatting
 const username = 'stuart' // TODO: replace this
 
+const sphero = require("sphero");
+const spheroId = "D8:AA:0C:8A:ED:4A"
+const orb = sphero(spheroId)
+
+const quadrant = process.argv[2];
+if(!quadrant) {
+	console.log("missing quadrant");
+	process.exit(1);
+}
+
+function quadrantAngleTransform(angle, quadrant) {
+	var transformed = angle;
+	if (quadrant === 1) {
+		// noop
+	} else {
+		transformed = transformed + (90)*(quadrant-1)
+	}
+	return (transformed % 360)
+}
+
+orb.connect(() => {
+	console.log("connected...")
+	console.log("calibrating...");
+	orb.startCalibration().delay(5000).then(() => {
+		orb.finishCalibration();
+		console.log("calibration successful!");
+		console.log("move mouse to control sphero");
+	});
+})
+
+orb.disconnect(() => {
+
+});
+
 const device = awsIot.device({
-  keyPath: 'certificates/stuarts_thing.private.key',
-  certPath: 'certificates/stuarts_thing.cert.pem',
-  caPath: 'certificates/root-CA.crt',
-  clientId: `${username}-subscribe`,
-      host: 'a2yujzh40clf9c.iot.us-east-2.amazonaws.com'
+	keyPath: 'certificates/stuarts_thing.private.key',
+	certPath: 'certificates/stuarts_thing.cert.pem',
+	caPath: 'certificates/root-CA.crt',
+	clientId: `${username}-subscribe`,
+    host: 'a2yujzh40clf9c.iot.us-east-2.amazonaws.com'
 });
 
 const topicName = 'stuart/mandala'
 
 device.on('connect', () => {
-  console.log('Subscriber client connected to AWS IoT cloud.\n');
-  device.subscribe(topicName);
-  console.log(`Subscribed to ${topicName}.`);
+	console.log('Subscriber client connected to AWS IoT cloud.\n');
+	device.subscribe(topicName);
+	console.log(`Subscribed to ${topicName}.`);
 });
 
 device.on('message', (topicName, payload) => {
-
-  let message = JSON.parse(payload.toString());
-
+	let message = JSON.parse(payload.toString());
+	orb.color('red')
+	speed = message.speed;
+	angle = message.angle;
+	stopped = message.stopped;
+	if(stopped) {
+		console.log('stopping');
+		orb.roll(0, 0);
+		orb.setRawMotors({
+			"lmode": "0x00",
+			"lpower": "0",
+			"rmode": "0x00",
+			"rpower": "0",
+		});
+	} else {
+		var newAngle = quadrantAngleTransform(angle, quadrant)
+		console.log(`Quadrant ${quadrant}: Moving in direction ${newAngle} with speed ${speed}`)
+		orb.roll(speed, newAngle).delay(800).then(() => {
+			orb.roll(0, newAngle)
+			stopped = false;
+		});
+	}
 });
--- mandala_publisher.js
+++ mandala_publisher.js
@@ -3,110 +3,133 @@
 const awsIot = require('aws-iot-device-sdk');
 const Mouse = require('node-mouse');
 var m = new Mouse();
-const username = 'stuart' // TODO: replace this
+const username = 'stuart'
 var stopped = false;
-
+var direction = 0;
 
 // AWS device
 const device = awsIot.device({
-  keyPath: 'certificates/stuarts_thing.private.key',
-  certPath: 'certificates/stuarts_thing.cert.pem',
-  caPath: 'certificates/root-CA.crt',
-  clientId: `${username}-publish`,
-      host: 'a2yujzh40clf9c.iot.us-east-2.amazonaws.com'
+	keyPath: 'certificates/stuarts_thing.private.key',
+	certPath: 'certificates/stuarts_thing.cert.pem',
+	caPath: 'certificates/root-CA.crt',
+	clientId: `${username}-publish`,
+	host: 'a2yujzh40clf9c.iot.us-east-2.amazonaws.com'
 });
 
+const sleep = (ms) => {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 // Mouse move
 
 const move = (event) => {
-  if(!stopped) {
-    var radian = Math.atan2( event.yDelta, event.xDelta);
-    var angle = (radian*180)/Math.PI;
-    var color = 'red';
+	if(!stopped) {
+		var radian = Math.atan2( event.yDelta, event.xDelta);
+		var angle = (radian*180)/Math.PI;
+		var color = 'red';
+		
+		switch(angle) {
+			case(0):
+				color = 'blue';
+				angle = 0;
+				break;
 
-    switch(angle) {
-		case(0):
-			color = 'blue';
-			angle = 0;
+			case(90):
+				color = 'blue';
+				
+				direction = 0
+				// angle = 90;
+				break;
+
+			case(180):
+				color = 'yellow';
+				direction = 270
+				// angle = 180;
+				break;
+
+			case(-90):
+				color = 'green';
+				// angle = 270;
+				direction = 180
+				break;
+
+			case(-180):
+				color = 'purple'
+				//angle = 180;
+				direction = 270
+				break;
+		default:
+			if (angle >= 0 && angle < 90) {
+				direction = 90 - angle
+				color = 'blue';
+			} else if(angle >= -90 && angle < 0) {
+				angle = 360 - Math.abs(angle)
+				direction = 90 + (360 - angle)
+				color = 'green';
+			} else if(angle <= -90 && angle > -180) {
+				angle = 90 + Math.abs(angle)
+				direction = 180 + (270 - angle)
+				color = 'purple';
+			} else if(angle >= 90 && angle < 180) {
+				direction = 360 - angle
+				color = 'yellow';
+			}
 			break;
+		}
 
-		case(90):
-			color = 'blue';
-			angle = 90;
-			break;
 
-		case(180):
-			color = 'yellow';
-			angle = 180;
-			break;
-
-		case(-90):
-			color = 'green';
-			angle = 270;
-			break;
-
-		case(-180):
-			color = 'purple'
-			angle = 180;
-			break;
-	default:
-  		if (angle >= 0 && angle < 90) {
-  			color = 'blue';
-  		} else if(angle >= -90 && angle < 0) {
-  			angle = 360 - Math.abs(angle)
-  			if(angle === 360) { angle = 0 }
-  			color = 'green';
-  		}
-  		else if(angle <= -90 && angle > -180) {
-  			angle = 90 + Math.abs(angle)
-  			color = 'purple';
-  		}
-  		else if(angle >= 90 && angle < 180) {
-  		  color = 'yellow';
-  		}
-  		break;
-  	}
-
-    stopped = true;
-
-  	var speed = ( Math.abs(event.yDelta) + Math.abs(event.xDelta) );
-    console.log(angle)
-    console.log(speed)
-
-    device.publish('stuart/mandala', JSON.stringify({
-        'color': color,
-        'angle': angle,
-        'speed': speed,
-        'stopped': false
-    }));
-  }
+		var speed = ( Math.abs(event.yDelta) + Math.abs(event.xDelta) );
+		// console.log(direction)
+		device.publish('stuart/mandala', JSON.stringify({
+			'color': color,
+			'angle': direction,
+			'speed': speed,
+			'stopped': false
+		}));
+	}
 };
+
+const debounce = (fn, time) => {
+  let timeout;
+
+  return function() {
+    const functionCall = () => fn.apply(this, arguments);
+    
+    clearTimeout(timeout);
+    timeout = setTimeout(functionCall, time);
+  }
+}
 
 const stop = () => {
-  if(!stopped) {
-    console.log('stopped... click again to resume');
-    stopped = true;
-    device.publish('stuart/mandala', JSON.stringify({
-        'stopped': true
-    }));
-  } else {
-    console.log('resumed');
-    device.publish('stuart/mandala', JSON.stringify({
-        'stopped': false
-    }));
-  }
+	if(!stopped) {
+		console.log('stopped... click again to resume');
+		stopped = true;
+		device.publish('stuart/mandala', JSON.stringify({
+			'stopped': true
+		}));
+	} else {
+		stopped = false;
+		console.log('resumed');
+		device.publish('stuart/mandala', JSON.stringify({
+			'stopped': false
+		}));
+	}
 };
 
-device.on('connect', () => {
-  console.log('Publisher client connected to AWS IoT cloud.\n');
-  m.on("move", (event) => {
-      console.log(event);
-      move(event);
-  })
-
-  m.on("left-down", (event) => {
-      console.log(event);
-      stop();
-  })
+m.on("mousemove", (event) => {
+	//move(event)
+	move(event, debounce((e) => {
+			console.log(e)
+	}, 1000));
 });
+
+m.on("click", (event) => {
+	console.log(event);
+	stop();
+});
+
+device.on('connect', () => {
+	console.log('Publisher client connected to AWS IoT cloud.\n');
+});
+
+
